a0b6decab251a4955782b639533968a78c654e3e
Sauswarya Mazumdar <sauswaryamazumdar@gmail.com>
2024-04-13 22:39:45 +0530

    This is the starting point

--- Blockchain/Backend/core/blockchain.py
+++ Blockchain/Backend/core/blockchain.py
@@ -5,7 +5,7 @@ Any violations may lead to legal action
 """
 import sys
 
-sys.path.append("/Users/Vmaha/Desktop/Bitcoin")
+sys.path.append("/Users/sausw/Desktop/Hexcoin")
 
 import copy
 import configparser
